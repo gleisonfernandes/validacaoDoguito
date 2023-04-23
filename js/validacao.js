ac35547a8b90108cfdc1cfa3b9659a4548f9b8d5
gleisonfernandes <gleison.fernandesb@gmail.com>
2023-04-22 23:10:41 -0300

    :sparkles: feat: adicionando as validações do formulário.

--- js/validacao.js
+++ js/validacao.js
@@ -4,10 +4,74 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    }else{
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    }
+}
+
+const tiposDeErro = [
+    'customError',
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+]
+
+const mensagensDeErro = {
+    nome:{
+        valueMissing: 'O campo nome não pode ser vazio.'
+    },
+    email:{
+        valueMissing: 'O campo email não pode ser vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha:{
+        valueMissing: 'O campo senha não pode ser vazio.',
+        patternMismatch: 'A senha deve conter entre 8 e 16 caracteres, deve conter pelo menos uma letra maiúcula e minúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento:{
+        valueMissing: 'O campo data de nascimento não pode ser vazio.',
+        customError: 'Você deve ser maior de 18 anos para se cadastrar.'
+    },
+    cpf:{
+        valueMissing: 'O campo cpf não pode ser vazio.',
+        customError: 'O CPF digitado não é válido.'
+    },
+    cep:{
+        valueMissing: 'O campo CEP não pode ser vazio.',
+        patternMismatch: 'O CEP digitado não é válido.'
+    },
+    logradouro:{
+        valueMissing: 'O campo logradouro não pode ser vazio.'
+    },
+    cidade:{
+        valueMissing: 'O campo cidade não pode ser vazio.'
+    },
+    estado:{
+        valueMissing: 'O campo estado não pode ser vazio.'
+    },
+    
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input){
+    let mensagem = '';
+
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]){
+            mensagem = mensagensDeErro[tipoDeInput][erro];
+        }
+    })
+
+    return mensagem;
 }
 
 function validaDataNascimento(input){
@@ -26,4 +90,70 @@ function maiorQue18(data){
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
     return dataMais18 <= dataAtual;
+}
+
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, '');
+    let mensagem = '';
+
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturCPF(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido.';
+    }
+    
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true;
+
+    valoresRepetidos.forEach(valor =>{
+        if(valor == cpf){
+            cpfValido = false;
+        }
+    })
+    return cpfValido;
+}
+
+function checaEstruturCPF(cpf){
+    const multiplicador = 10;
+
+    return checaDigitoVerificador(cpf, multiplicador);
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if(multiplicador >= 12){
+        return true;
+    }
+
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+        contador++;
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1);
+    }
+
+    return false;
+}
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11);
 }
